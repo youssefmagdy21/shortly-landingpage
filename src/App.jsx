91d3e76f152eccfcae6228d28f2d62ee6c1283a9
Youssef Magdy <yma.youssef.21@gmail.com>
2024-08-05 00:57:29 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="underline">Hello React!</h1>
+    </div>
+  );
+};
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary: {
+        cyan: "hsl(180, 66%, 49%)",
+        darkViolet: "hsl(257, 27%, 26%)",
+      },
+      secondary: {
+        red: "hsl(0, 87%, 67%)",
+      },
+      neutral: {
+        gray: "hsl(0, 0%, 75%)",
+        grayishViolet: "hsl(257, 7%, 63%)",
+        darkBlue: "hsl(255, 11%, 22%)",
+        darkViolet: "hsl(260, 8%, 14%)",
+      },
+      white: "#ffffff",
+      black: "#000000",
+    },
+    fontFamily: {
+      poppins: "Poppins, sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
