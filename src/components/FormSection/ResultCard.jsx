e5c2305c0502415d15118821a04462d3c8503256
Youssef Magdy <yma.youssef.21@gmail.com>
2024-08-12 02:29:06 +0300

    implemented button ui to display its state

--- src/components/FormSection/ResultCard.jsx
+++ src/components/FormSection/ResultCard.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 const ResultCard = ({ inputUrl, shortenedUrl }) => {
   const [isLinkCopied, setIsLinkCopied] = useState(false);
   const handleLinkCopy = async (e) => {
-    // clipboard functionality
     try {
       await navigator.clipboard.writeText(shortenedUrl);
       setIsLinkCopied(true);
@@ -16,21 +15,21 @@ const ResultCard = ({ inputUrl, shortenedUrl }) => {
       console.error(error);
     }
   };
+
   return (
     <div className="block gap-6 rounded-md bg-white shadow-sm md:flex md:items-center md:justify-between md:px-6 md:py-4">
       <p className="overflow-hidden text-ellipsis whitespace-nowrap border-b border-neutral-grayishViolet border-opacity-30 px-4 py-3 text-neutral-darkViolet md:border-none md:p-0">
         {inputUrl}
       </p>
-
       <div className="block px-4 py-3 md:flex md:items-center md:gap-6 md:p-0">
         <a
-          href="/"
+          href={shortenedUrl}
           className="text-primary-cyan transition-colors duration-300 ease-in-out hover:text-neutral-darkViolet"
         >
           {shortenedUrl}
         </a>
         <button
-          className="mt-3 block w-full rounded-md bg-primary-cyan py-[0.5rem] font-bold text-white transition-colors duration-300 ease-in-out hover:bg-primary-lightCyan md:mt-0 md:px-7 md:text-base"
+          className={`mt-3 block w-full rounded-md ${isLinkCopied ? "bg-primary-darkViolet" : "bg-primary-cyan hover:bg-primary-lightCyan"} py-[0.5rem] font-bold text-white transition-colors duration-300 ease-in-out md:mt-0 md:px-7 md:text-base`}
           onClick={handleLinkCopy}
         >
           {isLinkCopied ? "Copied!" : "Copy"}
@@ -39,4 +38,5 @@ const ResultCard = ({ inputUrl, shortenedUrl }) => {
     </div>
   );
 };
+
 export default ResultCard;
