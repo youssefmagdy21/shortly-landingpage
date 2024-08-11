dec9f9c8c31aac83b2f6d0867ac1bc9366811793
Youssef Magdy <yma.youssef.21@gmail.com>
2024-08-12 00:01:59 +0300

    finished form section to be fully responsive

--- src/components/FormSection/FormSection.jsx
+++ src/components/FormSection/FormSection.jsx
@@ -31,61 +31,41 @@ const FormSection = () => {
         </div>
 
         {/* output elements container */}
-        <div className="mt-5 space-y-5 text-sm">
+        <div className="mt-5 space-y-5 text-sm md:max-lg:text-base lg:text-lg">
           {/* element */}
-          <div className="block rounded-md bg-white shadow-sm">
-            <div className="border-b border-neutral-grayishViolet border-opacity-30 px-4 py-3">
-              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-neutral-darkViolet">
-                https://www.frontendmentor.io
-              </p>
-            </div>
-            <div className="px-4 py-3">
+          <div className="block gap-6 rounded-md bg-white shadow-sm md:flex md:items-center md:justify-between md:px-6 md:py-4">
+            <p className="overflow-hidden text-ellipsis whitespace-nowrap border-b border-neutral-grayishViolet border-opacity-30 px-4 py-3 text-neutral-darkViolet md:border-none md:p-0">
+              https://www.linkedin.com/company/frontend-mentor/
+            </p>
+
+            <div className="block px-4 py-3 md:flex md:items-center md:gap-6 md:p-0">
               <a
                 href="/"
                 className="text-primary-cyan transition-colors duration-300 ease-in-out hover:text-neutral-darkViolet"
               >
                 https://rel.ink/k4lKyk
               </a>
-              <button className="mt-3 block w-full rounded-md bg-primary-cyan py-[0.6em] font-bold text-white transition-colors duration-300 ease-in-out hover:bg-primary-lightCyan">
+              <button className="mt-3 block w-full rounded-md bg-primary-cyan py-[0.5rem] font-bold text-white transition-colors duration-300 ease-in-out hover:bg-primary-lightCyan md:mt-0 md:px-7 md:text-base">
                 Copy
               </button>
             </div>
           </div>
 
-          <div className="block rounded-md bg-white shadow-sm">
-            <div className="border-b border-neutral-grayishViolet border-opacity-30 px-4 py-3">
-              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-neutral-darkViolet">
-                https://www.frontendmentor.io/sometext/to/test/warpping
-              </p>
-            </div>
-            <div className="px-4 py-3">
+          {/* element */}
+          <div className="block gap-6 rounded-md bg-white shadow-sm md:flex md:items-center md:justify-between md:px-6 md:py-4">
+            <p className="overflow-hidden text-ellipsis whitespace-nowrap border-b border-neutral-grayishViolet border-opacity-30 px-4 py-3 text-neutral-darkViolet md:border-none md:p-0">
+              https://www.frontendmentor.io
+            </p>
+
+            <div className="block px-4 py-3 md:flex md:items-center md:gap-6 md:p-0">
               <a
                 href="/"
                 className="text-primary-cyan transition-colors duration-300 ease-in-out hover:text-neutral-darkViolet"
               >
                 https://rel.ink/k4lKyk
               </a>
-              <button className="mt-3 block w-full rounded-md bg-primary-darkViolet py-[0.6em] font-bold text-white">
+              <button className="mt-3 block w-full rounded-md bg-primary-darkViolet py-[0.5rem] font-bold text-white transition-colors duration-300 ease-in-out hover:bg-primary-lightCyan md:mt-0 md:px-7 md:text-base">
                 Copied!
-              </button>
-            </div>
-          </div>
-
-          <div className="block rounded-md bg-white shadow-sm">
-            <div className="border-b border-neutral-grayishViolet border-opacity-30 px-4 py-3">
-              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-neutral-darkViolet">
-                https://www.frontendmentor.io
-              </p>
-            </div>
-            <div className="px-4 py-3">
-              <a
-                href="/"
-                className="text-primary-cyan transition-colors duration-300 ease-in-out hover:text-neutral-darkViolet"
-              >
-                https://rel.ink/k4lKyk
-              </a>
-              <button className="mt-3 block w-full rounded-md bg-primary-cyan py-[0.6em] font-bold text-white transition-colors duration-300 ease-in-out hover:bg-primary-lightCyan">
-                Copy
               </button>
             </div>
           </div>
