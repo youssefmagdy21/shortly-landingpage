271a9fd808bf88ca50d21cc92852314f0fc35da8
Youssef Magdy <yma.youssef.21@gmail.com>
2024-08-15 01:11:24 +0300

    added animations on the mobile nav menu and close when clicked outside

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import MobileNav from "./MobileNav";
 import RoundedButton from "../RoundedButton";
 import logo from "/src/assets/images/logo.svg";
 import menuIcon from "/src/assets/images/icon-menu.svg";
+import { AnimatePresence } from "framer-motion";
 
 const Header = () => {
   const [openMenu, setOpenMenu] = useState(false);
@@ -30,7 +31,7 @@ const Header = () => {
       <div className="flex items-center px-6 py-8 md:px-16 md:py-11 lg:px-32 xl:px-40">
         {/* logo */}
         <span>
-          <a href="/" className="block">
+          <a href="/shortly-landingpage/" className="block">
             <img src={logo} alt="logo" />
           </a>
         </span>
@@ -63,7 +64,11 @@ const Header = () => {
           </button>
         </div>
       </div>
-      {openMenu && <MobileNav navItems={navItems} />}
+      <AnimatePresence>
+        {openMenu && (
+          <MobileNav navItems={navItems} closeMenu={() => setOpenMenu(false)} />
+        )}
+      </AnimatePresence>
     </header>
   );
 };
--- src/components/Header/MobileNav.jsx
+++ src/components/Header/MobileNav.jsx
@@ -1,29 +1,60 @@
+import { useRef } from "react";
+import { easeInOut, motion } from "framer-motion";
 import RoundedButton from "../RoundedButton";
 
-const MobileNav = ({ navItems }) => {
+const MobileNav = ({ navItems, closeMenu }) => {
+  const navMenuRef = useRef(null);
+  const closeMenuFromOutside = (e) => {
+    if (!navMenuRef.current.contains(e.target)) {
+      closeMenu();
+    }
+  };
   return (
     <div
-      className={`absolute left-0 right-0 z-50 mx-auto block max-w-[88%] rounded-xl bg-primary-darkViolet px-6 py-10 text-center text-base tracking-wide text-white md:hidden`}
+      className="absolute left-0 right-0 z-50 h-screen w-full bg-opacity-50 md:hidden"
+      onClick={closeMenuFromOutside}
     >
-      <nav className="mb-7 border-b border-neutral-grayishViolet border-opacity-20">
-        <ul className="">
-          {navItems.map((item) => (
-            <li key={item.id} className="mb-7">
-              <a href={item.link} className="block">
-                {item.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div>
-        <span className="mb-7 block">
-          <a href="#" className="block">
-            login
-          </a>
-        </span>
-        <RoundedButton textValue={"sign up"} link={"#"} />
-      </div>
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, x: 100 }}
+        transition={{
+          duration: 0.3,
+          type: easeInOut,
+        }}
+        className={`mx-auto block max-w-[88%] rounded-xl bg-primary-darkViolet px-6 py-10 text-center text-base tracking-wide text-white`}
+        ref={navMenuRef}
+      >
+        <nav className="mb-7 border-b border-neutral-grayishViolet border-opacity-20">
+          <ul className="">
+            {navItems.map((item, idx) => (
+              <motion.li
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{
+                  delay: 0.3 + 0.1 * idx,
+                  duration: 0.3,
+                  type: easeInOut,
+                }}
+                key={item.id}
+                className="mb-7"
+              >
+                <a href={item.link} className="block">
+                  {item.title}
+                </a>
+              </motion.li>
+            ))}
+          </ul>
+        </nav>
+        <div>
+          <span className="mb-7 block">
+            <a href="#" className="block">
+              login
+            </a>
+          </span>
+          <RoundedButton textValue={"sign up"} link={"#"} />
+        </div>
+      </motion.div>
     </div>
   );
 };
